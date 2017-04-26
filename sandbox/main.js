98d8cfceac92c536bc339b62729f73dd7e6aa5a1
Ganesh Ravichandran <gr2483@columbia.edu>
2017-04-26 02:39:46 -0400

    node functionality achieved

--- sandbox/main.js
+++ sandbox/main.js
@@ -1,117 +1,106 @@
 d3.json("graph.json", function(jsonData) {
 
-  panelNodes = {
-    "nodes": [
-      {"groupNum": 0, "x": 10, "y": 10},
-      {"groupNum": 1, "x": 10, "y": 40},
-      {"groupNum": 2, "x": 10, "y": 80},
-      {"groupNum": 3, "x": 10, "y": 120},
-      {"groupNum": 4, "x": 10, "y": 160}
-    ],
-    "links":
-    [{"source":1,"target":0}]
-    }
-
-  jsonData["nodes"].push.apply(jsonData["nodes"], panelNodes["nodes"]);
-
+  // whole canvas initialization
   var width = 960,
       height = 500;
       color = d3.scale.category20c();
 
   var svg = d3.select("body").append("svg")
       .attr("width", width)
-      .attr("height", height);
+      .attr("height", height)
+      .attr("class", "d3space");
 
-  var drag = d3.behavior.drag()
-   .on("drag", function(d, i) {
-     d.x += d3.event.dx
-     d.y += d3.event.dy
-     d3.select(this).attr("cx", d.x).attr("cy", d.y);
-     link.each(function(l, li) {
-       if (l.source == i) {
-         d3.select(this).attr("x1", d.x).attr("y1", d.y);
-       } else if (l.target == i) {
-         d3.select(this).attr("x2", d.x).attr("y2", d.y);
-       }
-     })
-   })
+  // left panel initialization
+  var initialNodeJSON = {
+    "nodes": [
+      {"groupNum": 0, "x": 10, "y": 10},
+      {"groupNum": 1, "x": 10, "y": 40},
+      {"groupNum": 2, "x": 10, "y": 80},
+      {"groupNum": 3, "x": 10, "y": 120},
+      {"groupNum": 4, "x": 10, "y": 160}],
+    "links": []};
 
-  var nodes = jsonData["nodes"],
-      links = jsonData["links"],
-      node = svg.selectAll(".node"),
-      link = svg.selectAll(".link");
+  // jsonData["nodes"].push.apply(jsonData["nodes"], initialNodeJSON["nodes"])
 
-  node = node
-    .data(nodes)
+  // json objects
+  var nodes = initialNodeJSON["nodes"],
+      links = initialNodeJSON["links"]
 
-  node
-  .enter().append("g")
-    .attr("class", "node")
-    .on("click", click);
+  var node = svg.selectAll(".node");
+  var link = svg.selectAll(".link");
 
-  // add the nodes
-  node.append("circle")
-      .attr("cx", function(d) {
-        return d.x
-      })
-      .attr("cy", function(d) {
-        return d.y
-      })
-      .attr("r", 5)
-      .style("fill", function(d) { return color(d.groupNum); })
-      .call(drag)
-      .on("dragend", function(d, i) {
+  // drag function for panel nodes
+  function nodeDrag(d, i) {
+    d.x += d3.event.dx
+    d.y += d3.event.dy
+    d3.select(this).attr("cx", d.x).attr("cy", d.y);
+  }
 
-        var replace_x = 10
-        var replace_y;
-        switch (d.groupNum) {
-          case 0:
-            replace_y = 10;
-            break;
-          case 1:
-            replace_y = 40;
-            break;
-          case 2:
-            replace_y = 80;
-            break;
-          case 3:
-            replace_y = 120;
-            break;
-          case 4:
-            replace_y = 160;
-            break;
-          }
-        nodes.push({"groupNum": d.groupNum, "x": replace_x, "y": replace_y});
-        refresh()
-       });
-   
+  function createNewPanelNode(d, i) {
+    if (d.x == 10) {
+
+      var node = svg.selectAll(".node");
+
+      var replace_x = 10
+      var replace_y;
+      switch (d.groupNum) {
+        case 0:
+          replace_y = 10;
+          break;
+        case 1:
+          replace_y = 40;
+          break;
+        case 2:
+          replace_y = 80;
+          break;
+        case 3:
+          replace_y = 120;
+          break;
+        case 4:
+          replace_y = 160;
+          break;
+        }
+      nodes.push({"groupNum": d.groupNum, "x": replace_x, "y": replace_y});
+      refresh()
+    }
+  }
+
+  function deleteNode(d, i) {
+    if (d.x < 20) {
+      d3.select(this).remove();
+      refresh();
+    }
+  }
+
+  // drag function that is used by panel nodes
+  var panelDrag = d3.behavior.drag()
+    .on("dragstart", createNewPanelNode)
+    .on("drag", nodeDrag)
+    .on("dragend", deleteNode);
+
   function refresh() {
 
-      node = node
-        .data(nodes)
+      node = node.data(nodes);
 
-      node
-      .enter().append("g")
-        .attr("class", "node")
-        .on("click", click);
+      // add the nodes
+      node.enter().append("circle")
+          .attr("cx", function(d) {
+            return d.x
+          })
+          .attr("cy", function(d) {
+            return d.y
+          })
+          .attr("r", 5)
+          .style("fill", "red")
+          .call(panelDrag)
 
-    // add the nodes
-    node.append("circle")
-        .attr("cx", function(d) {
-          return d.x
-        })
-        .attr("cy", function(d) {
-          return d.y
-        })
-        .attr("r", 5)
-        .style("fill", function(d) { return color(d.groupNum); })
-        .call(drag);
-   
-    // // add the text 
-    // node.append("text")
-    //     .attr("x", 12)
-    //     .attr("dy", ".35em")
-    //     .text(function(d) { return d.groupNum; });
+      node.exit().remove();
+
+    // // // add the text 
+    // // node.append("text")
+    // //     .attr("x", 12)
+    // //     .attr("dy", ".35em")
+    // //     .text(function(d) { return d.groupNum; });
 
     link = link.data(links);
 
@@ -140,11 +129,11 @@ d3.json("graph.json", function(jsonData) {
    
   // action to take on mouse click
   function click() {
-      d3.select(this).select("text").transition()
-          .duration(750)
-          .attr("x", 22)
-          .style("stroke", "lightsteelblue")
-          .style("stroke-width", ".5px")
-          .style("font", "20px sans-serif");
+      // d3.select(this).select("text").transition()
+      //     .duration(750)
+      //     .attr("x", 22)
+      //     .style("stroke", "lightsteelblue")
+      //     .style("stroke-width", ".5px")
+      //     .style("font", "20px sans-serif");
   }
 });
